a0f404b8fc3e7fa8376efa48f0bb1a3e4f9fe565
kumulonimbus91 <nenadvukojevic@hotmail.com>
2021-07-14 01:31:47 +0200

    Initial commit

---
+++ app/src/main/java/com/nenadvukojevic/quizapp/Question.kt
@@ -0,0 +1,25 @@
+package com.nenadvukojevic.quizapp
+
+data class Question (
+
+
+    val id: Int,
+    val Question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+
+
+
+
+
+
+
+)
+
+
+
+
